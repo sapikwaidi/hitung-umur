5738dfbdf0c55c666cbd14c6e8f9d6ad26fb1665
sapiwaidi <sapikwaidi@gmail.com>
2023-04-09 18:39:03 +0700

    create counting age

---
+++ script.js
@@ -0,0 +1,22 @@
+function getAge() {
+    var date = document.getElementById('birthday').value;
+ 
+    if(date === ""){
+        alert("Please complete the required field!");
+    }else{
+        var today = new Date();
+        var birthday = new Date(date);
+        var year = 0;
+        if (today.getMonth() < birthday.getMonth()) {
+            year = 1;
+        } else if ((today.getMonth() == birthday.getMonth()) && today.getDate() < birthday.getDate()) {
+            year = 1;
+        }
+        var age = today.getFullYear() - birthday.getFullYear() - year;
+ 
+        if(age < 0){
+            age = 0;
+        }
+        document.getElementById('result').innerHTML = age;
+    }
+}
